cef5f31cce704296caae450697092f8d8240b229
Alistair Smith <alistairsmith01@gmail.com>
2020-11-06 23:21:32 +0000

    fucking delete it all

--- src/up.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Upperface a string
- * @param string The string you want to upperface
- */
-export function upperFace(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
